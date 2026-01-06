f0701483c3d65a141f378f24320d64be35c3c60e
Jamitz <jamitz440@pm.me>
2026-01-06 23:09:14 +0000

    Init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,24 @@
 // @ts-check
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: "https://fitzy.dev",
+  integrations: [mdx(), sitemap()],
+
+  markdown: {
+    shikiConfig: {
+      theme: "catppuccin-macchiato",
+      wrap: true,
+    },
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
